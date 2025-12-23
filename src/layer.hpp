1faaee16fca8e8c47e9675b833b26c687105b65d
nalinally <obama1632@gmail.com>
2025-12-23 09:25:39 +0900

    hppのレイヤクラスを作った

--- src/layer.hpp
+++ src/layer.hpp
@@ -1,7 +1,22 @@
 #pragma once
 
+#include <cstddef>
+#include <cassert>
+#include <vector>
+
 namespace layer {
+
+class Layer{
+
+ public:  
+  Layer();
+  std::vector<double> getNodes();
+
+ private:
+  std::vector<double> nodes;
+
+};
 
 extern int tameshi;
 
-}
+}  // namespace layer
